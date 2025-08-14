39332450fa86c12496d0861cdb0179698da5e0da
phihanh-qg <phamphikhanh2707@gmail.com>
2025-08-14 07:09:13 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -11,3 +11,28 @@ function changeImage(smallImg) {
 function resetImage() {
   document.getElementById("mainImage").src = originalSrc;
 }
+document.addEventListener("DOMContentLoaded", function () {
+  var nav = document.querySelector("nav ul");
+  var adminName = localStorage.getItem("user");
+
+  if (adminName) {
+    var li = document.createElement("li");
+    li.innerHTML = `
+      <a href="#">👤 ${adminName}</a>
+      <ul class="dropdown-menu">
+        <li><a href="#" id="logoutBtn">Đăng xuất</a></li>
+      </ul>
+    `;
+    li.classList.add("dropdown");
+    nav.replaceChild(li, nav.lastElementChild);
+
+    // Gán sự kiện đăng xuất
+    document.getElementById("logoutBtn").addEventListener("click", function (e) {
+      e.preventDefault();
+      localStorage.removeItem("admin");
+      localStorage.removeItem("user");
+      alert("Đã đăng xuất!");
+      window.location.href = "index.html";
+    });
+  }
+});
